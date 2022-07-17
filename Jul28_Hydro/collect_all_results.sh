0468c521a054182f8999178320b8b69b2a6d1937
Lipei Du <lpdu318@gmail.com>
2022-07-16 23:36:30 -0400

    Add more xml files and updates

--- Jul28_Hydro/collect_all_results.sh
+++ Jul28_Hydro/collect_all_results.sh
@@ -6,20 +6,19 @@ echo "collect results into " $folder " ..."
 
 mkdir -p $folder
 
-mv meanpT_estimators_eta_-0.5_0.5.dat $folder/
-mv meanpT_estimators_tau_0.64.dat $folder/
-mv meanpT_estimators_tau_1.04.dat $folder/
-mv eccentricities_evo_ed_tau_0.64.dat $folder/
-mv eccentricities_evo_ed_tau_1.04.dat $folder/
-mv eccentricities_evo_eta_-0.5_0.5.dat $folder/
-mv eccentricities_evo_nB_tau_0.64.dat $folder/
-mv eccentricities_evo_nB_tau_1.04.dat $folder/
-mv momentum_anisotropy_tau_0.64.dat $folder/
-mv momentum_anisotropy_tau_1.04.dat $folder/
-mv momentum_anisotropy_eta_-0.5_0.5.dat $folder/
-mv inverse_Reynolds_number_eta_-0.5_0.5.dat $folder/
-mv surface_MUSIC.dat $folder/
-mv evolution_all_xyeta_MUSIC.dat $folder/
-mv test_out.dat $folder/
-mv hadron_list.dat $folder/
-mv particle_samples.bin $folder/
+mv meanpT_estimators_eta_-0.5_0.5.dat $folder/ 2> /dev/null
+mv meanpT_estimators_tau_*.dat $folder/ 2> /dev/null
+mv eccentricities_evo_ed_tau_*.dat $folder/ 2> /dev/null
+mv eccentricities_evo_eta_-0.5_0.5.dat $folder/ 2> /dev/null
+mv eccentricities_evo_nB_tau_*.dat $folder/ 2> /dev/null
+mv momentum_anisotropy_tau_*.dat $folder/ 2> /dev/null
+mv momentum_anisotropy_eta_-0.5_0.5.dat $folder/ 2> /dev/null
+mv inverse_Reynolds_number_eta_-0.5_0.5.dat $folder/ 2> /dev/null
+mv global_conservation_laws.dat $folder/ 2> /dev/null
+mv surface_MUSIC.dat $folder/ 2> /dev/null
+mv evolution_all_xyeta_MUSIC.dat $folder/ 2> /dev/null
+mv test_out.dat $folder/ 2> /dev/null
+mv hadron_list.dat $folder/ 2> /dev/null
+mv particle_samples.bin $folder/ 2> /dev/null
+
+mv $folder hydro_session/$folder
